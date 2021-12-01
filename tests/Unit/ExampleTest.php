77cb2f35056a4778ad4bc764774d24b95dc2a30d
jamiestansfield <jstansfield@rawnet.com>
2021-12-01 14:45:28 +0000

    initial commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Support\Facades\Artisan;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        tap()
+        $this->assertTrue(true);
+    }
+}
